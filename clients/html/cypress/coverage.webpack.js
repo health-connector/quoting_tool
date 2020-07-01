45c124490ff3c9fa2e19a50397cc830e430d8090
Mark Goho <markgoho@gmail.com>
2020-07-01 12:48:31 -0400

    use updated instanbul babel integration

--- clients/html/cypress/coverage.webpack.js
+++ clients/html/cypress/coverage.webpack.js
@@ -1,12 +1,15 @@
-console.log(require('path').join(__dirname, '..', 'src/app'));
-
 module.exports = {
   module: {
     rules: [
       {
         test: /\.(js|ts)$/,
-        loader: 'istanbul-instrumenter-loader',
-        options: { esModules: true },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // presets: ['@babel/preset-env'],
+            plugins: ['babel-plugin-istanbul']
+          }
+        },
         enforce: 'post',
         include: [require('path').join(__dirname, '..', 'src/app')],
         exclude: [/\.(e2e|spec)\.ts$/, /node_modules/, /(ngfactory|ngstyle)\.js/]
