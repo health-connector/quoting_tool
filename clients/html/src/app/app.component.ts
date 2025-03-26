f358f5ef169787481969a1713f0809a64780dfe4
Brad Bodine <bbodine1@gmail.com>
2025-03-25 19:23:50 -0500

    Refactor application structure to utilize standalone components, removing the AppModule. Update routing configuration and enhance component imports in AppComponent. Adjust test specifications to reflect changes in component structure.

--- clients/html/src/app/app.component.ts
+++ clients/html/src/app/app.component.ts
@@ -2,12 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { CustomDateParserFormatter } from './custom_date_parser_formatter';
 import Swal from 'sweetalert2';
+import { FooterComponent } from './footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { NgIf } from '@angular/common';
 
 @Component({
-  providers: [{provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter}],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    providers: [{ provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter }],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [NgIf, HeaderComponent, RouterOutlet, FooterComponent]
 })
 export class AppComponent implements OnInit {
   showIENotSupportiveMessage = false;
--- clients/html/src/main.ts
+++ clients/html/src/main.ts
@@ -1,13 +1,51 @@
 import 'hammerjs';
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { SharedModule } from './app/shared/shared.module';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { provideRouter, Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'employer-details',
+        pathMatch: 'full'
+    },
+    {
+        path: 'employer-details',
+        loadChildren: () => import("./app/employer-details/employer-details.module").then((m) => m.EmployerDetailsModule)
+    },
+    {
+        path: 'employer-details/health',
+        loadChildren: () => import("./app/employer-details/employer-details-health/health.module").then((m) => m.HealthModule)
+    },
+    {
+        path: 'employer-details/dental',
+        loadChildren: () => import("./app/employer-details/employer-details-dental/dental.module").then((m) => m.DentalModule)
+    }
+];
+
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(CommonModule, BrowserModule, FormsModule, ReactiveFormsModule, NgbModule, SweetAlert2Module.forRoot(), SharedModule),
+        provideAnimations(),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideRouter(routes)
+    ]
+})
   .catch(err => console.log(err));
