23559aa368a18300718d927b718e6d6e597799cc
Brad Bodine <bbodine1@gmail.com>
2025-04-03 12:17:04 -0500

    Update environment configuration files to remove trailing slashes from API URLs for consistency. Adjusted `cataract_api` and `envApi` properties in both production and development environment files.

--- clients/html/src/environments/environment.ts
+++ clients/html/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  envApi: 'http://localhost:3000/',
-  cataract_api: 'http://localhost:3001/'
+  envApi: 'http://localhost:3000',
+  cataract_api: 'http://localhost:3001',
 };
 
 /*
